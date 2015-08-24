8eac52e05148fd4a9b0c4311272276f91b077d11
Letladi <letladi@letladi.com>
2015-08-24 10:53:26 +0000

    Fixing usernameAvailable function since autopublish package was removed

--- server/users/users.js
+++ server/users/users.js
@@ -1,6 +1,16 @@
 Meteor.methods({
   verifyUserEmail: function() {
     Accounts.sendVerificationEmail(Meteor.userId());
+  },
+  usernameAvailable: function(username) {
+    check(username, String);
+    var user = Meteor.users.findOne({ username: username });
+    if ( user && user.username && 
+        (user.username.toLowerCase() === username.toLowerCase()) ) {
+      return false; 
+    } else  {
+      return true;
+    }
   }
 });
 
