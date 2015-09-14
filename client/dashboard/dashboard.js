2bec25589230bf835b54c78e11e60307b044fdaa
Letladi <letladi@letladi.com>
2015-09-14 14:08:36 +0000

    Getting ready for file uploads

--- client/dashboard/dashboard.js
+++
@@ -1,7 +0,0 @@
-Template.dashboard.onRendered(function() {
-  $('.container').addClass('dashboard');
-});
-
-Template.dashboard.onDestroyed(function() {
-  $('.container').removeClass('dashboard');
-});
--- client/users/users.js
+++
@@ -1,7 +0,0 @@
-Template.userTerms.onRendered(function() {
-  $('.container').addClass('user-terms');
-});
-
-Template.userTerms.onDestroyed(function() {
-  $('.container').removeClass('user-terms');
-});
