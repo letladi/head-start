72d5af95e5e8050febeaf0244ba99a790b00a4ad
Letladi <sletladi@gmail.com>
2015-11-12 15:17:00 +0200

    styled header and cleaned up header events

--- client/templates/header/header.js
+++ client/templates/header/header.js
@@ -1,37 +1,4 @@
 Template.header.events({
-  'click .btn': function(ev) {
-    if ($('.nav').hasClass('hide-nav')) {
-      $('.nav').fadeIn(300, function() {
-        $('.nav').toggleClass('hide-nav');
-      }); 
-    } else {
-      $('.nav').fadeOut(300, function() {
-        $('.nav').toggleClass('hide-nav');
-      });
-    };
-    var btnContainerClass = $(ev.target).attr('class');
-   // btnContainerClass = btnContainerClass.split(' ')[0];
-    //console.log(btnContainerClass);
-    $('.menu-btn .btn').toggleClass('fa-bars').toggleClass('fa-times');
-  },
-  'click .js-show-sub-menu': function(ev, tmpl) {
-    tmpl.$(ev.target)
-      .removeClass('fa-angle-down')
-      .addClass('fa-angle-up')
-      .removeClass('js-show-sub-menu')
-      .addClass('js-hide-sub-menu')
-      .next()
-      .fadeIn(300);
-  },
-  'click .js-hide-sub-menu': function(ev, tmpl) {
-    tmpl.$(ev.target)
-      .addClass('fa-angle-down')
-      .removeClass('fa-angle-up')
-      .addClass('js-show-sub-menu')
-      .removeClass('js-hide-sub-menu')
-      .next()
-      .fadeOut(300);
-  },
   'click .logout': function() {
     Meteor.logout();
     Router.go('login');
